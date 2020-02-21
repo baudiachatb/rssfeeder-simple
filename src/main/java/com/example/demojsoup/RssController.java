6c48e593d8096ea973a8d4aeb222a68bad360c16
Bau Nguyen Van <baudcctak54@gmail.com>
2020-02-22 02:09:57 +0700

    update

--- src/main/java/com/example/demojsoup/RssController.java
+++ src/main/java/com/example/demojsoup/RssController.java
@@ -16,17 +16,39 @@ public class RssController {
 
 
     @GetMapping(value = "/rsscontent")
-    public List<RssContent> getRssContent() {
-        return facadeService.rssContentService.getAllRssContent();
+    public List<RssContent> getRssContent(@RequestParam(value = "page") String page) {
+        switch (page) {
+            case "vnexpress":
+                return facadeService.vnExpressGetContentService.getAllRssContent("https://vnexpress.net/rss");
+            case "dantri":
+                return facadeService.dantriGetContentService.getAllRssContent("https://dantri.com.vn/rss.htm");
+            default:
+                return facadeService.vnExpressGetContentService.getAllRssContent("https://vnexpress.net/rss");
+        }
     }
 
     @GetMapping("/content")
-    public List<Content> getContent(@RequestParam(value = "url") String url) {
-        return facadeService.contentService.getListPost("https://vnexpress.net/" + url);
+    public List<Content> getContent(@RequestParam(value = "url") String url, @RequestParam(value = "page") String page) {
+        switch (page) {
+            case "dantri":
+                return facadeService.dantriGetContentService.getListPost("https://dantri.com.vn/" + url);
+            case "vnexpress":
+                return facadeService.vnExpressGetContentService.getListPost("https://vnexpress.net/" + url);
+            default:
+                return facadeService.vnExpressGetContentService.getListPost("https://vnexpress.net/" + url);
+        }
+
     }
 
     @GetMapping("/post")
-    public PostContent getPost(@RequestParam(value = "url") String url) {
-        return facadeService.postContentService.getContent(url);
+    public PostContent getPost(@RequestParam(value = "url") String url, @RequestParam(value = "page") String page) {
+        switch (page) {
+            case "dantri":
+                return facadeService.dantriGetContentService.getContent(url);
+            case "vnexpress":
+                return facadeService.vnExpressGetContentService.getContent(url);
+            default:
+                return facadeService.vnExpressGetContentService.getContent(url);
+        }
     }
 }
--- src/main/java/entitys/FacadeService.java
+++ src/main/java/entitys/FacadeService.java
@@ -1,11 +1,9 @@
 package entitys;
 
-import entitys.Content.ContentService;
-import entitys.PostContent.PostContentService;
-import entitys.rsscontent.RssContentService;
+import services.DantriGetContentService;
+import services.VnExpressGetContentService;
 
 public class FacadeService {
-    public ContentService contentService = new ContentService();
-    public PostContentService postContentService = new PostContentService();
-    public RssContentService rssContentService = new RssContentService();
+    public VnExpressGetContentService vnExpressGetContentService = new VnExpressGetContentService();
+    public DantriGetContentService dantriGetContentService = new DantriGetContentService();
 }
